eaff2c1d00932cc06ca1a88d4951aebaf36699f4
Saw Xue Zheng <sawxuezheng01@gmail.com>
2016-09-15 20:18:55 -0400

    Update create_course_dialog.java

--- src/Form/create_course_dialog.java
+++ src/Form/create_course_dialog.java
@@ -1,7 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * Author: Saw Xue Zheng
+ * Description: Interface the Dialog Box for Create Course Buttons
+ * 
  */
 package Form;
 import Form.Classes.*;
