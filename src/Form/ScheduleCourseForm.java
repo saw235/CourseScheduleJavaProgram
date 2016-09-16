82005f0389ba77cdb840f4988d54c899fb066a71
Saw Xue Zheng <sawxuezheng01@gmail.com>
2016-09-15 20:17:26 -0400

    Added Descriptions

--- src/Form/ScheduleCourseForm.java
+++ src/Form/ScheduleCourseForm.java
@@ -1,8 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * Author: Saw Xue Zheng
+ * Description: Interfaces the ScheduleCourse Form
  */
+ 
 package Form;
 import Form.Classes.*;
 import java.io.ByteArrayOutputStream;
