aabec6f89759d4f8b56d79a700a5dfd5b6965d19
Saw Xue Zheng <sawxuezheng01@gmail.com>
2016-09-15 20:16:22 -0400

    Added Descriptions

--- src/Form/MainFrame.java
+++ src/Form/MainFrame.java
@@ -1,7 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * Author: Saw Xue Zheng
+ * Description: Interfaces the Main Form
+ * 
  */
 package Form;
 
